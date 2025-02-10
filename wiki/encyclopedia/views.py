0c597e4501cae6d2e9f524d1ee76bac9ba51c6f5
Innocent Anozie <iygeal@gmail.com>
2025-02-10 23:06:47 +0100

    NEW: Modified entry_page() function to let title match entry without regard to case, using next() and generator expression

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -8,3 +8,21 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry_page(request, title):
+    """This function fetches the content of the entry page"""
+
+    entries = util.list_entries()
+    formatted_title = next(
+        (entry for entry in entries if entry.upper() == title.upper()), title)
+
+    content = util.get_entry(formatted_title)
+
+    if content is None:
+        return render(request, "encyclopedia/error.html", {
+            "message": "The requested page was not found."
+        })
+    return render(request, "encyclopedia/entry_page.html", {
+        "title": formatted_title,
+        "content": content
+    })
